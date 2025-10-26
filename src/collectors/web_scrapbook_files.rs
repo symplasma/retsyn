37fbe8d28632f2e237db557897b402877d902a5f
Elio Grieco <grieco@egx.com>
2025-10-25 19:17:31 -0700

    feat: Add indexing of WebScrapbook Archives

--- src/collectors/web_scrapbook_files.rs
+++ src/collectors/web_scrapbook_files.rs
@@ -1,10 +1,14 @@
+use dom_query::Document;
 use ignore::WalkBuilder;
 
 use crate::{
     config::PathList,
     index_entry::index_entry::{IndexEntry, IndexPath, IndexPathSender},
 };
-use std::path::{Path, PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 pub(crate) const WEB_SCRAPBOOK_FILES_SOURCE: &str = "web_scrapbook_files";
 
@@ -64,26 +68,42 @@ impl WebScrapbookFiles {
         println!("attempting to convert path to entry...");
 
         // TODO properly handle non UTF-8 file contents
-        // TODO handle very large files efficiently
-        // let body = fs::read_to_string(&path).unwrap_or_default();
-        let body = "body".to_string();
+        // TODO handle very large files efficiently, maybe switch to a streaming library
+        let body = fs::read_to_string(&path).unwrap_or_default();
 
         // Extract title according to priority rules
-        let title = Self::extract_title(&body, &path);
+        let title = Self::extract_title(&body);
 
         // convert the body to markdown before returning
+        let doc = Document::from(body);
+        // TODO ensure that any extraneous elements like styling and classes are not passed along
+        // TODO run this through the readability library
+        let markdown_body = doc.md(None);
 
         IndexEntry::new(
             WEB_SCRAPBOOK_FILES_SOURCE.to_owned(),
             path.to_string_lossy().to_string(),
             title,
-            body,
+            markdown_body.to_string(),
         )
     }
 
-    fn extract_title(body: &str, path: &&Path) -> String {
+    fn extract_title(body: &str) -> String {
         // read the first line and pull the title from
         // <!DOCTYPE html><html lang="en" data-scrapbook-source="https://www.theguardian.com/business/2023/aug/28/phoenix-microchip-plant-biden-union-tsmc" data-scrapbook-create="20231215012632582" data-scrapbook-title="‘They would not listen to us’: inside Arizona’s troubled chip plant | Business | The Guardian">
-        "title".to_string()
+        let title = body
+            .lines()
+            .next()
+            .and_then(|l| {
+                Document::fragment(l)
+                    .select("html")
+                    .attr("data-scrapbook-title")
+                    .map(|a| a.to_string())
+            })
+            .unwrap_or("UNKNOWN_TITLE".to_owned());
+
+        // TODO add fallback here to pull title from the page, don't forget to clean it up (squeeze and trim whitespace, make it one line)
+
+        title
     }
 }
